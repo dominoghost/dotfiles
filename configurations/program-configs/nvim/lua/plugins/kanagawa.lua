72f28475340b019fe1f81cdee3e8e2f19e149887
parallax <Wake8234@proton.me>
2026-03-11 14:23:43 -0400

    Removed kanagawa

--- configurations/program-configs/nvim/lua/plugins/kanagawa.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"rebelot/kanagawa.nvim",
-	priority = 1000,
-	--	config = function()
-	--		local kanagawa = require("kanagawa")
-	--		kanagawa.setup({ transparent = true })
-	--		kanagawa.load("dragon")
-	--	end,
-}
